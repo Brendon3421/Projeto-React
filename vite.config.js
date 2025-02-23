905a1df4e921e344e11ef9a7d96f0f9ba4edf43e
Brendon3421 <brendonrroger@gmail.com>
2025-02-22 21:21:45 -0300

    Subindo alteracao

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/Projeto-React', // Certifique-se de que a base esteja configurada corretamente
+    base: '/Projeto-React/', // Certifique-se de que a base esteja configurada corretamente
 });
